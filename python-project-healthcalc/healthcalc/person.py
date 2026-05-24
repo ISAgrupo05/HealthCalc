01aebc77b860e76b58d7b1725ba30fc2e78b8a34
Berica99 <belenrios@uma.es>
2026-05-24 12:02:09 +0200

    feat: Modificar clase Person #168

--- python-project-healthcalc/healthcalc/person.py
+++ python-project-healthcalc/healthcalc/person.py
@@ -7,10 +7,14 @@ class Person:
         weight: float,
         height: float,
         gender: Gender,
+        waist: float,
+        hip: float,
         age: int = 0
     ):
 
         self.weight = weight
         self.height = height
         self.gender = gender
+        self.waist = waist
+        self.hip = hip
         self.age = age
